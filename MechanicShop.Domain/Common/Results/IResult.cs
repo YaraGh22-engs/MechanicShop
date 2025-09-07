e9d0b4aa108f9a9ac8c035068d56a4924d286fe7
YaraGh <yaraesmaiel123@gmail.com>
2025-09-07 11:27:57 -0500

    Result Pattern

---
+++ MechanicShop.Domain/Common/Results/IResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicShop.Domain.Common.Results
+{
+    public interface IResult
+    {
+        List <Error> errors { get; }
+        bool IsSuccess { get; }
+    }
+    public interface IResult<out TValue> : IResult
+    {
+        TValue Value { get; }
+    }
+}
