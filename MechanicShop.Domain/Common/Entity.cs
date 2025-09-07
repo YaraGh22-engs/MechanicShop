f84ed3ddc5dd033131e158f47abc20a754589e26
YaraGh <yaraesmaiel123@gmail.com>
2025-09-07 07:46:53 -0500

    Domain Events

--- MechanicShop.Domain/Common/Entity.cs
+++ MechanicShop.Domain/Common/Entity.cs
@@ -10,11 +10,26 @@ namespace MechanicShop.Domain.Common
     {
         public Guid Id { get; }
 
+        private readonly List<DomainEvent> _domainEvents = [];
+
         protected Entity() { }
 
         protected Entity(Guid id)
         {
             id = Id == Guid.Empty ? Guid.Empty : id;
+        }
+
+        public void AddDomainEvent(DomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+        public void RemoveDomainEvent(DomainEvent domainEvent)
+        {
+            _domainEvents.Remove(domainEvent);
+        }
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
         }
     }
 }
