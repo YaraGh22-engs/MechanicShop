93e8affdfd21330c34c1cebb76a9c0eb159e76ab
YaraGh <yaraesmaiel123@gmail.com>
2025-09-10 08:07:42 -0500

    Application Layer : Queries

---
+++ MechanicShop.Application/Features/Customers/Queries/GetCustomers/GetCustomersQueryHandler.cs
@@ -0,0 +1,26 @@
+using MechanicShop.Application.Common.Interfaces;
+using MechanicShop.Application.Features.Customers.Dtos;
+using MechanicShop.Application.Features.Customers.Mappers;
+using MechanicShop.Domain.Common.Results;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MechanicShop.Application.Features.Customers.Queries.GetCustomers;
+
+public class GetCustomersQueryHandler(IAppDbContext context
+    )
+    : IRequestHandler<GetCustomersQuery, Result<List<CustomerDto>>>
+{
+    private readonly IAppDbContext _context = context;
+
+    public async Task<Result<List<CustomerDto>>> Handle(GetCustomersQuery query, CancellationToken ct)
+    {
+        //get customers with them vehicles
+        var customers = await _context.Customers.Include(c => c.Vehicles).AsNoTracking().ToListAsync(ct);
+
+        //convert to DTO
+        return customers.ToDtos();
+    }
+}
